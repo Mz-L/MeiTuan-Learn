bed518d687d23ebb964d6847d6be20feeced4ec5
Mz-L <1462541455@qq.com>
2017-08-19 16:14:09 +0800

    逛一逛界面的实现

--- pages/Shop/ShopPage.js
+++ pages/Shop/ShopPage.js
@@ -5,22 +5,39 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  WebView
 } from 'react-native';
 
 import ShopBar from '../../components/ShopBar';
 
 export default class ShopPage extends Component {
+  state={
+    url:'http://i.meituan.com/topic/scene/1?cevent=imt%2Fhomepage%2Fhomeguide4'
+  };
+
   render() {
     return (
-        <View>
+        <View style={styles.viewStyle}>
           <ShopBar></ShopBar>
-          <Text>ShopPage</Text>
+          <WebView
+            ref={'webView'}
+            automaticallyAdjustContentInsets={false}
+            source={{uri: this.state.url}}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            decelerationRate="normal"
+            startInLoadingState={true}
+            scalesPageToFit={true}
+          />
         </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-
+  viewStyle:{
+    backgroundColor:'#F5FCFF',
+    flex:1
+  }
 });
